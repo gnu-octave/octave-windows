79c53d0a1ea43264305b0294b3993ae3ce6c3b9d
John Donoghue <devnull@localhost>
2018-04-11 07:49:00 -0400

    maint: update copyright for files modified for 1.3.0 release

--- src/__COM__.cc
+++ src/__COM__.cc
@@ -1,10 +1,10 @@
 /*
 
-  Copyright (C) 2006 Michael Goffioul <michael.goffioul@swing.be>
+  Copyright (C) 2006-2018 Michael Goffioul <michael.goffioul@swing.be>
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
+  the Free Software Foundation; either version 3 of the License, or
   (at your option) any later version.
 
   This program is distributed in the hope that it will be useful,
