c304ae715d5e49598732b0ea5a27db46693ce0f0
John Donoghue <devnull@localhost>
2019-04-08 13:06:44 -0400

    Update licenses on older files * examples/mat2xls.m: update license for gpl3
    * src/grab_win32part.cc, src/grab.cc: add license header
    
    * src/image.m.in: update license for gpl3

--- examples/mat2xls.m
+++ examples/mat2xls.m
@@ -1,8 +1,8 @@
-## Copyright (C) 2007  Michael Goffioul <michael.goffioul@swing.be>
+## Copyright (C) 2007-2019  Michael Goffioul <michael.goffioul@swing.be>
 ##
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 2 of the License, or
+## the Free Software Foundation; either version 3 of the License, or
 ## (at your option) any later version.
 ##
 ## This program is distributed in the hope that it will be useful,
--- src/grab.cc
+++ src/grab.cc
@@ -1,4 +1,24 @@
 /*
+  Copyright (C) 1997,2003 Andy Adler <adler@ncf.ca>
+  Copyright (C) 2001 Laurent Mazet <mazet@crm.mot.com>
+  Copyright (C) 2019 John Donoghue <john.donoghue@ieee.org>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+/*
  * Get graphical coordinates from screen
  * 
  * This contains bits of code hacked from the
